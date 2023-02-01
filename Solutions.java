a7a5ff578177feb426c67946092beae914c9a70f
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-02-01 11:11:54 -0600

    Should be done

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,87 @@
 public class Solutions {
-    public static void main(String[] args) {
+    //max3
+    public static int max3(int a, int b, int c){
+        int largest = 0;
         
+        if(a>b){
+            largest = a;
+        }else{
+            largest =b;
+        }
+        if(largest<c){
+            largest = c;
+        }
+        return largest;
+    }
+    //This is for max3 overloaded
+    public static double max3ol(double a, double b, double c){
+        double largest = 0;
+        if(a>b){
+            largest = a;
+        }else{
+            largest =b;
+        }
+        if(largest<c){
+            largest = c;
+        }
+        return largest;
+    }
+
+    public static boolean odd(Boolean a, Boolean b, Boolean c){
+        boolean num=true;
+        int tof = 0;
+        if(a==num){
+            tof += 1;
+        }
+        if(b==num){
+            tof += 1;
+        }
+        if(c==num){
+            tof += 1;
+        }
+        if((tof%2)==1){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    //testing \/
+    public static boolean majority(boolean a, boolean b, boolean c){
+        int maj = 0;
+        boolean num=true;
+        if(a==num){
+            maj += 1;
+        }
+        if(b==num){
+            maj += 1;
+        }
+        if(c==num){
+            maj += 1;
+        }
+        if(maj>=2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static double trigIdentity(double x){
+        //sin^2(`x`) + cos^2(`x`)
+        double sin1 = Math.sin( Math.sqrt(x)); 
+        double cos1 = Math.cos(Math.sqrt(x));
+        double ans = sin1 +cos1;
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+       System.out.println(max3(0,9,0));
+       System.out.println(max3ol(0.99555, 0.498849, 0.9854));
+       System.out.println(odd(true, true, true));
+       System.out.println(majority(true, false, true));
+       System.out.println(trigIdentity(2));
     }
 }
+
+// javac Solutions.java; java Solutions
